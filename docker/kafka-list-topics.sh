e6f109c47ceff7f168d05ed902b4bd76578b8fec
Hans Schuell <hans.schuell@gmx.de>
2023-08-19 00:26:52 +0200

    Support for describe added to script

--- docker/kafka-list-topics.sh
+++ docker/kafka-list-topics.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker exec -it ${1:-kafka-1} kafka-topics \
+docker exec ${1:-kafka-1} kafka-topics \
     --bootstrap-server ${1:-kafka-1}:${2:-9092} \
-    --list
+    --list > _topics
 
-# kafka-topics --describe --topic job-accepted --bootstrap-server ${1:-kafka-1}:${2:-9092}
+cat _topics
+
+cat _topics | grep -v __ | while read topic; do
+  docker exec --tty ${1:-kafka-1} kafka-topics \
+      --bootstrap-server ${1:-kafka-1}:${2:-9092} \
+      --describe --topic $topic
+done
