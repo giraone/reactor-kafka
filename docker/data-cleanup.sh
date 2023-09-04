c3298019385a78b6298780447ae3f6df38aa55ef
Hans Schuell <hans.schuell@gmx.de>
2023-09-04 08:52:21 +0200

    Volumes for prometheus/loki/grafana

--- docker/data-cleanup.sh
+++ docker/data-cleanup.sh
@@ -2,3 +2,6 @@
 
 rm -rf ${CONTAINER_DATA:-./data}/kafka-1
 rm -rf ${CONTAINER_DATA:-./data}/zk-1
+rm -rf ${CONTAINER_DATA:-./data}/prometheus
+rm -rf ${CONTAINER_DATA:-./data}/loki
+rm -rf ${CONTAINER_DATA:-./data}/grafana
--- docker/data-setup.sh
+++ docker/data-setup.sh
@@ -5,3 +5,6 @@ mkdir -m 777 ${CONTAINER_DATA:-./data}/kafka-1
 mkdir -m 777 ${CONTAINER_DATA:-./data}/zk-1
 mkdir -m 777 ${CONTAINER_DATA:-./data}/zk-1/data
 mkdir -m 777 ${CONTAINER_DATA:-./data}/zk-1/log
+mkdir -m 777 ${CONTAINER_DATA:-./data}/prometheus
+mkdir -m 777 ${CONTAINER_DATA:-./data}/loki
+mkdir -m 777 ${CONTAINER_DATA:-./data}/grafana
